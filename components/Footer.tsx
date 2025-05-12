c33f4245f5a11d5e1fab8986f34b2ec246edd1e1
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2025-05-12 17:05:33 -0400

    Remove about page and fix unused variables

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 const Footer = () => {
   return (
     <footer className="w-full p-4 dark:bg-[#111111] md:flex md:items-center md:justify-between">
