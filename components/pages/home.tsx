d4dc7b18af87bc8910d0566d2b25249550a4cf81
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2026-08-15 11:11:22 -0400

    Update home.tsx

--- components/pages/home.tsx
+++ components/pages/home.tsx
@@ -104,9 +104,8 @@ export default function Home() {
                   Everything you need is already here
                 </h1>
                 <p className="mt-6 tracking-tight text-gray-400 text-lg/7">
-                  Blue0x is not just a blockchain. There is an entire decentralized
-                  ecosystem, built-in and ready to go! All transactions are executed on-chain and
-                  fully transparent.
+                  "Blue0x is more than just a blockchain—it's an entire decentralized ecosystem, 
+                  built in and ready to go! All transactions are executed on-chain and remain fully transparent."
                 </p>
               </div>
             </div>
