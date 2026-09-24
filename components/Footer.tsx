3a321981923d446886f1f9fa98991c7435520939
Blue0x <3836882+theBlue0x@users.noreply.github.com>
2026-09-24 15:15:07 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const Footer = () => {
   return (
-    <footer className="p-4 w-full md:flex md:justify-between md:items-center bg-[#0c0c0c]">
+    <footer className="p-4 w-full md:flex md:justify-between md:items-center bg-[#000000]">
     </footer>
   );
 };
